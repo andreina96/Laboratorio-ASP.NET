6e03dec3c632ef3b4dbbc6a65b06efd5957959b0
Andreína Alvarado <andreinaraccoon@hotmail.com>
2016-09-16 04:09:20 -0600

    Commit 3

---
+++ Laboratorio-ASP.NET/Laboratorio-ASP.NET/Controllers/ClientesV2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio_ASP.NET.Models;
+
+namespace Laboratorio_ASP.NET.Controllers
+{
+    public class ClientesV2Controller : Controller
+    {
+        LaboratorioMVCEntities db = new LaboratorioMVCEntities();
+
+        public ActionResult Index()
+        {
+            ModeloIntermedio modelo = new ModeloIntermedio();
+            modelo.listaClientes = db.Cliente.ToList();
+            modelo.listaCuentas = db.Cuenta.ToList();
+            modelo.listaTelefonos = db.Telefono.ToList();
+            return View(modelo);        }
+    }
+}
---
+++ Laboratorio-ASP.NET/Laboratorio-ASP.NET/Models/ModeloIntermedio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio_ASP.NET.Models
+{
+    public class ModeloIntermedio
+    {
+        public Cliente modeloCliente { get; set; }
+        public Cuenta modeloCuenta { get; set; }
+        public Telefono modeloTelefono { get; set; }
+        public List<Cliente> listaClientes = new List<Cliente>();
+        public List<Cuenta> listaCuentas = new List<Cuenta>();
+        public List<Telefono> listaTelefonos = new List<Telefono>();
+    }
+}
